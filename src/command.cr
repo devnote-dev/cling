fc265b96ccbc6256417e6694b10478853fd5465f
devnote-dev <inferno0745@gmail.com>
2022-09-19 01:11:59 +0100

    fix(command): typings, set procs, exec setup

--- src/command.cr
+++ src/command.cr
@@ -13,41 +13,40 @@ module CLI
     property? hidden : Bool
     property? inherit_borders : Bool
     property? inherit_options : Bool
-
+    getter on_error : Exception ->
+    getter on_missing_args : Array(String) ->
+    getter on_unknown_args : Array(String) ->
+    getter on_missing_opts : Array(String) ->
+    getter on_unknown_opts : Array(String) ->
     @help_template : String?
-    @on_error : Exception ->
-    @on_missing_args : Array(String) ->
-    @on_unknown_args : Array(String) ->
-    @on_missing_opts : Array(String) ->
-    @on_unknown_opts : Array(String) ->
 
     def initialize(*, aliases : Array(String)? = nil, usage : Array(String)? = nil,
                    @header : String? = nil, @summary : String? = nil, @description : String? = nil,
                    @footer : String? = nil, @parent : Command? = nil, children : Array(Command)? = nil,
-                   arguments : Array(Argument)? = nil, options : Array(Option)? = nil,
+                   arguments : Hash(String, Argument)? = nil, options : Hash(String, Option)? = nil,
                    @hidden : Bool = false, @inherit_borders : Bool = true,
                    @inherit_options : Bool = true)
       @aliases = aliases || [] of String
       @usage = usage || [] of String
-      @children = children || [] of Command
-      @arguments = arguments || [] of Argument
-      @options = options || [] of Option
+      @children = children || {} of String => Command
+      @arguments = arguments || {} of String => Argument
+      @options = options || {} of String => Option
 
-      @on_error = ->(ex) { raise ex }
+      @on_error = ->(ex : Exception) { raise ex }
 
-      @on_missing_args = ->(args) do
+      @on_missing_args = ->(args : Array(String)) do
         raise %(Missing required argument#{"s" if args.size > 1}: #{args.join(", ")})
       end
 
-      @on_unknown_args = ->(args) do
+      @on_unknown_args = ->(args : Array(String)) do
         raise %(Unknown argument#{"s" if args.size > 1}: #{args.join(", ")})
       end
 
-      @on_missing_opts = ->(opts) do
+      @on_missing_opts = ->(opts : Array(String)) do
         raise %(Missing required option#{"s" if opts.size > 1}: #{opts.join(", ")})
       end
 
-      @on_unknown_opts = ->(opts) do
+      @on_unknown_opts = ->(opts : Array(String)) do
         raise %(Unknown option#{"s" if opts.size > 1}: #{opts.join(", ")})
       end
     end
@@ -71,6 +70,7 @@ module CLI
     def add_command(command : Command.class) : Nil
       cmd = command.new parent: self
       cmd.setup
+
       raise "Duplicate command '#{cmd.name}'" if @children.has_key? cmd.name
 
       if cmd.inherit_borders?
@@ -92,26 +92,28 @@ module CLI
     end
 
     def add_option(long : String, *, desc : String? = nil, required : Bool = false,
-                   default : Option::Value::Type = nil) : Nil
+                   has_value : Bool = false, default : Option::Value::Type = nil) : Nil
       raise "Duplicate flag option '#{long}'" if @options.has_key? long
-      @options[long] = Option.new(long, nil, desc, required, default)
+
+      @options[long] = Option.new(long, nil, desc, required, has_value, default)
     end
 
     def add_option(short : Char, long : String, *, desc : String? = nil, required : Bool = false,
-                   default : Option::Value::Type = nil) : Nil
+                   has_value : Bool = false, default : Option::Value::Type = nil) : Nil
       raise "Duplicate flag option '#{long}'" if @options.has_key? long
 
-      if op = @options.find { |o| o.short == short }
+      if op = @options.values.find { |o| o.short == short }
         raise "Flag '#{op.long}' already has the short option '#{short}'"
       end
 
-      @options[long] = Option.new(long, short, desc, required, default)
+      @options[long] = Option.new(long, short, desc, required, has_value, default)
     end
 
     def execute(input : Array(String), *, parser : Parser? = nil) : Nil
+      setup
       parser ||= Parser.new(input, Parser::Options.new)
       results = parser.parse
-      # Executor.new(self).handle(results)
+      Executor.new(self).handle(results)
     end
 
     def pre_run(args, options) : Nil
