e55a673a4a9ebbcaabe3088ffdd6da1513cda7a0
devnote-dev <inferno0745@gmail.com>
2022-10-23 19:54:55 +0100

    feat: document option and value

--- src/cli/value.cr
+++ src/cli/value.cr
@@ -1,4 +1,5 @@
 module CLI
+  # Represents a value for an argument or option.
   struct Value
     alias Type = String | Number::Primitive | Bool | Nil # | Array(Type)
 
@@ -7,10 +8,12 @@ module CLI
     def initialize(@raw : Type)
     end
 
+    # :inherit:
     def to_s(io : IO) : Nil
       @raw.to_s io
     end
 
+    # Returns the size of the value if it is an array or hash, otherwise raises an exception.
     def size : Int32
       case value = @raw
       when Array
@@ -22,22 +25,27 @@ module CLI
       end
     end
 
+    # :inherit:
     def ==(other : Type) : Bool
       @raw == other
     end
 
+    # Returns the value as a `String`.
     def as_s : String
       @raw.as(String)
     end
 
+    # Returns the value as an `Int`.
     def as_i : Int
       @raw.to_s.to_i
     end
 
+    # Returns the value as a `Float`.
     def as_f : Float
       @raw.to_s.to_f
     end
 
+    # Returns the value as a `Bool`.
     def as_bool : Bool
       if @raw.is_a? Bool
         @raw.as(Bool)
@@ -51,42 +59,51 @@ module CLI
       end
     end
 
+    # Returns the value as `nil`.
     def as_nil : Nil
       @raw.as(Nil)
     end
 
+    # Returns the value as an `Array`. Note that this does not change the type of the array.
     def as_a : Array(Type)
       @raw.as(Array)
     end
 
     {% for base in %w(8 16 32 64 128) %}
+    # Returns the value as an `Int{{ base }}`.
     def as_i{{ base.id }} : Int{{ base.id }}
       @raw.as(Int{{ base.id }}).to_i{{ base.id }}
     end
 
+    # Returns the value as an `Int{{ base }}` or `nil`.
     def as_i{{ base.id }}? : Int{{ base.id }}?
       @raw.as?(Int{{ base.id }}).try &.to_i{{ base.id }}?
     end
 
+    # Returns the value as a `UInt{{ base }}`.
     def as_u{{ base.id }} : UInt{{ base.id }}
       @raw.as(UInt{{ base.id }}).to_u{{ base.id }}
     end
 
+    # Returns the value as a `UInt{{ base }}` or `nil`.
     def as_u{{ base.id }}? : UInt{{ base.id }}?
       @raw.as?(UInt{{ base.id }}).try &.to_u{{ base.id }}?
     end
     {% end %}
 
     {% for base in %w(32 64) %}
+    # Returns the value as a `Float{{ base }}`.
     def as_f{{ base.id }} : Float{{ base.id }}
       @raw.as(Float{{ base.id }}).to_f{{ base.id }}
     end
 
+    # Returns the value as a `Float{{ base }}` or `nil`.
     def as_f{{ base.id }}? : Float{{ base.id }}?
       @raw.as?(Float{{ base.id }}).try &.to_f{{ base.id }}?
     end
     {% end %}
 
+    # Indexes the value if the value is an array, otherwise raises an exception.
     def [](index : Int32) : Type
       case value = @raw
       when Array
@@ -96,6 +113,8 @@ module CLI
       end
     end
 
+    # Attempts to index the value if the value is an array or returns `nil`, otherwise raises an
+    # exception.
     def []?(index : Int32) : Type
       case value = @raw
       when Array
@@ -105,6 +124,7 @@ module CLI
       end
     end
 
+    # :ditto:
     def [](index : Range) : Type
       case value = @raw
       when Array
@@ -114,6 +134,7 @@ module CLI
       end
     end
 
+    # :ditto:
     def []?(index : Range) : Type
       case value = @raw
       when Array
@@ -123,6 +144,7 @@ module CLI
       end
     end
 
+    # Indexes the value if the value is a hash, otherwise raises an exception.
     def [](key : String) : Type
       case value = @raw
       when Hash
@@ -132,10 +154,12 @@ module CLI
       end
     end
 
-    def []?(key : String) : Type
+    # Attempts to index the value if the value is a hash or returns `nil`, otherwise raises an
+    # exception.
+    def []?(key : String) : Type?
       case value = @raw
       when Hash
-        value[index]
+        value[index]?
       else
         raise "Cannot get index of type #{value.class}"
       end
