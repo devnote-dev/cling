467feaa50ac06be7c27f3e60c97f570e84911b9e
devnote-dev <devnote.dev75@gmail.com>
2023-03-05 23:34:30 +0000

    feat(spec): add command specs

---
+++ spec/command_spec.cr
@@ -0,0 +1,46 @@
+require "./spec_helper"
+
+private class MainCommand < Cling::Command
+  def setup : Nil
+    @name = "main"
+
+    add_argument "first", required: true
+    add_argument "second"
+    add_option 's', "skip"
+  end
+
+  def pre_run(arguments : Cling::Arguments, options : Cling::Options) : Bool
+    !options.has?("skip")
+  end
+
+  def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+    arguments.get "first"
+    arguments.get "second"
+  end
+end
+
+describe Cling::Command do
+  it "executes the pre_run only" do
+    command = MainCommand.new
+    command.execute %w(--skip)
+  end
+
+  it "fails on missing arguments" do
+    command = MainCommand.new
+    expect_raises Cling::CommandError do
+      command.execute %w()
+    end
+  end
+
+  it "executes without errors" do
+    command = MainCommand.new
+    command.execute %w(foo bar)
+  end
+
+  it "raises on unknown values" do
+    command = MainCommand.new
+    expect_raises Cling::ValueNotFound do
+      command.execute %w(foo)
+    end
+  end
+end
