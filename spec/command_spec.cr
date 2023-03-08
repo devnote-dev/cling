bd18fd34297327864b5c476600660048f151ec19
devnote-dev <devnote.dev75@gmail.com>
2023-03-08 23:56:51 +0000

    spec(command): improve spec coverage

--- spec/command_spec.cr
+++ spec/command_spec.cr
@@ -1,6 +1,6 @@
 require "./spec_helper"
 
-private class MainCommand < Cling::Command
+private class TestArgsCommand < Cling::Command
   def setup : Nil
     @name = "main"
 
@@ -19,29 +19,74 @@ private class MainCommand < Cling::Command
   end
 end
 
+private class TestOptionsCommand < Cling::Command
+  def setup : Nil
+    @name = "main"
+
+    add_option "foo"
+    add_option "double-foo", required: true
+    add_option 'b', "bar", type: :single, required: true
+    add_option 'n', "num", type: :array, default: %w()
+  end
+
+  def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+    puts options
+    options.get? "foo"
+    options.get "double-foo"
+    options.get('b').as_s
+    options.get('n').as_a
+  end
+end
+
 describe Cling::Command do
   it "executes the pre_run only" do
-    command = MainCommand.new
+    command = TestArgsCommand.new
     command.execute %w(--skip)
   end
 
   it "fails on missing arguments" do
-    command = MainCommand.new
+    command = TestArgsCommand.new
     expect_raises Cling::CommandError do
       command.execute %w()
     end
   end
 
   it "executes without errors" do
-    command = MainCommand.new
+    command = TestArgsCommand.new
     command.execute %w(foo bar)
     command.execute %w(foo bar baz qux)
   end
 
   it "raises on unknown values" do
-    command = MainCommand.new
+    command = TestArgsCommand.new
     expect_raises Cling::ValueNotFound do
       command.execute %w(foo)
+    end
+  end
+
+  it "fails on missing options" do
+    command = TestOptionsCommand.new
+    expect_raises Cling::CommandError do
+      command.execute %w()
+    end
+  end
+
+  it "executes without errors" do
+    command = TestOptionsCommand.new
+    command.execute %w(--double-foo --bar=true)
+  end
+
+  it "fails on unknown options" do
+    command = TestOptionsCommand.new
+    expect_raises Cling::CommandError do
+      command.execute %w(--double-foo --double-bar)
+    end
+  end
+
+  it "fails on invalid options" do
+    command = TestOptionsCommand.new
+    expect_raises Cling::ExecutionError do
+      command.execute %w(--foo=true --double-foo)
     end
   end
 end
